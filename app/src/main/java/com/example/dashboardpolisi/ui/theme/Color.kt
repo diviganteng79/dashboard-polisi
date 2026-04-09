7faea984d967b04b2b0d8deb3e08a65b438fd3c0
login_jsc <diviganteng79@gmail.com>
2026-04-09 11:07:09 +0700

    Create Color.kt

---
+++ app/src/main/java/com/example/dashboardpolisi/ui/theme/Color.kt
@@ -0,0 +1,7 @@
+package com.example.dashboardpolisi.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val BluePrimary = Color(0xFF0A3A80)
+val BlueSecondary = Color(0xFF2F5DA9)
+val SurfaceLight = Color(0xFFF5F7FB)
