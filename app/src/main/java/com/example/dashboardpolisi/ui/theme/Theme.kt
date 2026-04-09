6fd5fc6117428cfaad7b8c918585d45688accc59
login_jsc <diviganteng79@gmail.com>
2026-04-09 11:09:01 +0700

    Create Theme.kt

---
+++ app/src/main/java/com/example/dashboardpolisi/ui/theme/Theme.kt
@@ -0,0 +1,28 @@
+package com.example.dashboardpolisi.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+private val LightColors = lightColorScheme(
+    primary = BluePrimary,
+    secondary = BlueSecondary,
+    background = SurfaceLight
+)
+
+private val DarkColors = darkColorScheme(
+    primary = BlueSecondary
+)
+
+@Composable
+fun DashboardPolisiTheme(
+    darkTheme: Boolean = false,
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colorScheme = if (darkTheme) DarkColors else LightColors,
+        typography = Typography,
+        content = content
+    )
+}
